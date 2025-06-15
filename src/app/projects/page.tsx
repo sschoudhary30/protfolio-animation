eae13ff38db6246389921da561da28c8d2cbdc61
suresh <suresh12345x@gmail.com>
2025-06-15 18:27:13 +0530

    react 18 to 19

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -10,9 +10,63 @@ export default function ProjectsPage() {
         <Heading> Project - The hard work!!</Heading>
 
         <SubHeading as="h2">
-          I&apos;m a software engineer with a passion for building scalable
-          antialiased efficient system. I&apos;m currently working as a software
-          engineer at nice software solution.
+          Every project I build is a blend of precision and passion – from
+          smooth UI flows{" "}
+          <span
+            style={{
+              backgroundColor: "black",
+              borderRadius: "5px",
+              padding: "3px",
+              margin: "2px",
+              fontWeight: "bold",
+            }}
+          >
+            🧩
+          </span>{" "}
+          to scalable backend logic
+          <span
+            style={{
+              backgroundColor: "lightgray",
+              borderRadius: "5px",
+              padding: "3px",
+              margin: "2px",
+              fontWeight: "bold",
+            }}
+          >
+            🚀
+          </span>
+          . Here’s a glimpse into the products, platforms, and ideas I’ve
+          brought to life.
+        </SubHeading>
+
+        <SubHeading as="h2">
+          <span
+            style={{
+              backgroundColor: "black",
+              borderRadius: "5px",
+              padding: "3px",
+              margin: "2px",
+              fontWeight: "bold",
+            }}
+          >
+            {" "}
+            🌙
+          </span>{" "}
+          From late-night ideas to polished builds 💻, these projects are where
+          I experiment 🧪, iterate 🔁, and push boundaries – one deploy at a
+          time{" "}
+          <span
+            style={{
+              backgroundColor: "lightgray",
+              borderRadius: "5px",
+              padding: "3px",
+              margin: "2px",
+              fontWeight: "bold",
+            }}
+          >
+            🚢
+          </span>
+          .
         </SubHeading>
 
         <Projects />
